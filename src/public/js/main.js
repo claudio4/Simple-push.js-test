4294ebd6b9649976aa932889928cc42813b238bb
Claudio4 <me@claudio4.com>
2016-08-12 20:27:55 +0100

    Adds buttons and styles.
    Adds a button for open notficaion.
    
    Adds a button for close all notifications.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,4 +4,42 @@ window.addEventListener("load", function(e){
         icon: "img/js.png",
         timeout: 3000
     });
+    document.getElementById("open").addEventListener("click", function(e){
+        e.stopPropagation();
+        Push.create("You did it!", {
+            body:"But you can't click me",
+            icon: "img/js.png",
+            timeout: 4000,
+            onClick: function() {
+                archivement();
+                this.close();
+            }
+        })
+    })
+    document.getElementById("close").addEventListener("click", function(e) {
+        e.stopPropagation();
+        Push.clear();
+    })
+    function archivement(){
+        Push.create("Archivement Unlocked!", {
+            body: "Law breaker",
+            icon: "img/law.png",
+            timeout: 4000,
+            onClick: function(){
+                pleaseStop();
+                this.close();
+            }
+        });
+    function pleaseStop() {
+        Push.create("Stop!", {
+            body: "Stop or take the consequences",
+            icon: "img/stop.png",
+            timeout: 10000,
+            onClick: function(){
+                window.location = "about:blank";
+                this.close();
+            }
+        });
+    }
+    }
 })
